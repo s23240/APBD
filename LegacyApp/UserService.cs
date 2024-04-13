451ac617becce8601789ff2f30a2f9cadf6b8017
s23240 <127010629+s23240@users.noreply.github.com>
2024-04-13 13:20:57 +0200

    cwiczenie 2

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -1,26 +1,58 @@
 ﻿using System;
+using System.Linq;
 
 namespace LegacyApp
 {
     public class UserService
     {
-        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        private static bool NameValidator(string name)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            return !string.IsNullOrEmpty(name) && name.All(char.IsLetter);
+        }
+        
+        private static bool EmailValidator(string email)
+        {
+            try
+            {
+                var address = new System.Net.Mail.MailAddress(email);
+                return address.Address == email;
+            }
+            catch
             {
                 return false;
             }
-
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
+        }
+        
+        private static int CalculateAge(DateTime dateOfBirth)
+        {
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            return age;
+        }
+        
+        private static bool AgeValidator(DateTime dateOfBirth)
+        {
+            var age = CalculateAge(dateOfBirth);
+            const int legalAge = 21;
+            return age >= legalAge;
+        }
+        
+        
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!NameValidator(firstName) || !NameValidator(lastName))
+            {
+                return false;
+            }
 
-            if (age < 21)
+            if (!EmailValidator(email))
+            {
+                return false;
+            }
+
+
+            if (!AgeValidator(dateOfBirth))
             {
                 return false;
             }
